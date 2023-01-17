c504440e6dcf492c29f405ef92a99bcbc201fd57
Ваня Дубинников <ivan.dubinnikov@gmail.com>
2023-01-17 14:44:31 +0200

    First commit; plug in firebase; add note components and add some views to note components

---
+++ notes/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebaseConfig:{
+    apiKey: "AIzaSyCxvp5DmYqJoKhY7POlVbR7B04YCGuK80A",
+    authDomain: "fire-notes-98d66.firebaseapp.com",
+    projectId: "fire-notes-98d66",
+    storageBucket: "fire-notes-98d66.appspot.com",
+    messagingSenderId: "845379981822",
+    appId: "1:845379981822:web:59f79c612888b5cde430af"
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
